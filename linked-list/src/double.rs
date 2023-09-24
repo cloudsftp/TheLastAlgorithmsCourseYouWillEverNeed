f6cd8adf4794e771fbc1243c2f01284204b49d47
Fabian Weik <fabian.weik@protonmail.com>
2023-09-24 16:15:42 +0200

    complete inserts operations

--- linked-list/src/double.rs
+++ linked-list/src/double.rs
@@ -1,4 +1,4 @@
-use std::{borrow::BorrowMut, cell::RefCell, rc::Rc};
+use std::{cell::RefCell, fmt::Display, rc::Rc};
 
 use anyhow::{anyhow, Error};
 
@@ -32,7 +32,7 @@ where
         }
     }
 
-    fn get(&self, i: usize) -> Result<T, Error> {
+    fn walk_to_index(&self, i: usize) -> Result<Rc<RefCell<Node<T>>>, Error> {
         let mut curr = self.head.clone();
 
         for _ in 0..i {
@@ -46,11 +46,14 @@ where
 
         match curr {
             None => Err(anyhow!("index {i} is out of bounds")),
-            Some(link) => {
-                let ret = link.borrow().val;
-                Ok(ret)
-            }
+            Some(link) => Ok(link),
         }
+    }
+
+    fn get(&self, i: usize) -> Result<T, Error> {
+        let node = self.walk_to_index(i)?;
+        let ret = node.borrow().val;
+        Ok(ret)
     }
 
     fn append(&mut self, val: T) {
@@ -67,12 +70,112 @@ where
         } else {
             let tail = self.tail.take();
             node.prev = tail.clone();
+            let link = new_link(node);
             if let Some(tail) = tail {
-                tail.as_ref().borrow_mut().next = new_link(node);
+                tail.as_ref().borrow_mut().next = link.clone();
             }
+            self.tail = link;
         }
 
         self.len += 1;
+    }
+
+    fn prepend(&mut self, val: T) {
+        let mut node = Node {
+            val,
+            next: None,
+            prev: None,
+        };
+
+        if self.len == 0 {
+            let node = new_link(node);
+            self.head = node.clone();
+            self.tail = node;
+        } else {
+            let head = self.head.take();
+            node.next = head.clone();
+            let link = new_link(node);
+            if let Some(head) = head {
+                head.as_ref().borrow_mut().prev = link.clone();
+            }
+            self.head = link;
+        }
+
+        self.len += 1;
+    }
+
+    fn insert_at(&mut self, i: usize, val: T) -> Result<(), Error> {
+        if i < 0 || i > self.len {
+            return Err(anyhow!("index {i} is out of bounds"));
+        }
+
+        if i == 0 {
+            self.prepend(val);
+            return Ok(());
+        }
+        if i == self.len {
+            self.append(val);
+            return Ok(());
+        }
+
+        let next = self.walk_to_index(i)?;
+        let prev = next
+            .as_ref()
+            .borrow_mut()
+            .prev
+            .take()
+            .ok_or(anyhow!("internal problem"))?
+            .clone();
+
+        let node = Some(Rc::new(RefCell::new(Node {
+            val,
+            next: Some(next.clone()),
+            prev: Some(prev.clone()),
+        })));
+
+        prev.borrow_mut().next = node.clone();
+        next.borrow_mut().prev = node;
+        Ok(())
+    }
+}
+
+impl<T> Display for Node<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}", self.val))?;
+        Ok(())
+    }
+}
+
+impl<T> Display for List<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut curr = self.head.clone();
+        f.write_str("[")?;
+
+        let mut i = 0;
+        loop {
+            match curr {
+                None => break,
+                Some(node) => {
+                    if i > 0 {
+                        f.write_str(", ")?;
+                    }
+                    i += 1;
+
+                    let node = node.borrow();
+                    f.write_fmt(format_args!("{}", node))?;
+                    curr = node.next.clone();
+                }
+            }
+        }
+        f.write_str("]")?;
+
+        Ok(())
     }
 }
 
@@ -98,6 +201,30 @@ mod test {
         l.append(0);
         l.append(1);
 
-        assert!(list_equals(&l, &[0, 1]))
+        assert!(list_equals(&l, &[0, 1]));
+    }
+
+    #[test]
+    fn prepend() {
+        let mut l = List::<i32>::new();
+        l.prepend(0);
+        l.prepend(1);
+
+        assert!(list_equals(&l, &[1, 0]))
+    }
+
+    #[test]
+    fn insert() {
+        let mut l = List::<i32>::new();
+        l.append(0);
+        l.append(1);
+        l.append(2);
+        l.append(3);
+        l.append(4);
+
+        l.insert_at(2, 10).unwrap();
+
+        println!("{l}");
+        assert!(list_equals(&l, &[0, 1, 10, 2, 3, 4]))
     }
 }
